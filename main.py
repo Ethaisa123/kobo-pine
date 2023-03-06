0fe4acfe828d022013c8a315f645f1ee4c8d0a24
Ethan van Wijk <ethanvanwijknz@gmail.com>
2023-03-07 10:47:15 +1300

    Update main.py

--- main.py
+++ main.py
@@ -8,19 +8,37 @@ manga_list = api.get_manga_list(title = title)
 m_list = manga_list[0] # chooses the first version (english i think?)
 print(m_list.title)
 
-#gets all the chapter and volume information (we only need the chapter id's)
-m_chapters = api.get_manga_volumes_and_chapters(manga_id = m_list.manga_id)
-volumes = list(m_chapters.keys())
-""" SOMEHOW TURN THIS MASIVE DICTIONARY INTO USABLE CHAPTER IDS"""
-#print(volumes[1])
-print(m_chapters["12"]["chapters"][0])
+#gets all the chapter and volume information (we only need the chapter id's and names)
+m_chapters_all = api.get_manga_volumes_and_chapters(manga_id = m_list.manga_id)
 
-for i in range (0, len(volumes)):
-    print(volumes[i])
+""" SOMEHOW TURN THIS MASIVE DICTIONARY INTO USABLE CHAPTER IDS WITH NAMES"""
+#sets up the final dict with all the ids
+chapt_names_ids = {}
+
+vol_names_ids = {}
+#finds the names for all the volumes
+volume_names = list(m_chapters_all.keys())
+
+for i in range (0, len(volume_names)):
+    #gets all the information for chapters from the big list
+    chapters_in_volume = m_chapters_all[volume_names[i]]["chapters"]
+    #gets all the names for the volumes for the lists
+    chapter_names = list(chapters_in_volume.keys())
+
+    for i in range (0, len(chapter_names)):
+        #gets the chapter name
+        chap = chapter_names[i]
+        #gets the chapter information to ids
+        chapt_dict_ids = chapters_in_volume[str(chap)]
+        #finds the ids from the dict using the key "id"
+        chapt_ids = chapt_dict_ids["id"]
+        #adds the names and ids to an easy to acess dictionary
+        chapt_names_ids[chap] = chapt_ids
 
 
-#gets the cahpter information with the list of chapter id's
-m_chapter = api.get_chapter(chapter_id = "015979c8-ffa4-4afa-b48e-3da6d10279b0")
+#gets the cahpter information with the list of chapter id's created previosly
+m_chapter = api.get_chapter(chapter_id = chapt_names_ids["20"])
 
 #gets all the page images from the chosen chapter
 m_pages = m_chapter.fetch_chapter_images()
+print(m_pages)

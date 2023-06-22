bb712774864c713aed26d6672adddcda6954276a
Ethan van Wijk <ethanvanwijknz@gmail.com>
2023-06-22 19:40:16 +1200

    varios fixes, added oneshot support,

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import os
 import shutil
-from secoundary import volumes, chap_dict, chap_pdf_location, Title, chap_premerge, folder, folder_2_location
+from secoundary import volumes, chap_dict, chap_pdf_location, Title, chap_premerge, folder, oneshot_
 import fitz
 #python -m pip install --upgrade pymupdf
 vol_folder = Title + "_volumes"
-os.mkdir(vol_folder)
+try:
+    os.mkdir(vol_folder)
+except:
+    FileExistsError
+    shutil.rmtree(vol_folder)
+    os.mkdir(vol_folder)
 
 main_dirct = os.getcwd()
 
 #condenses all chapters into a single file if no 
+if oneshot_ == True:
+    os.chdir(main_dirct)
+    print(chap_pdf_location + ("\\{}_Oneshot.pdf" .format(Title)))
+    shutil.move(chap_pdf_location + ("\\{}_Oneshot.pdf" .format(Title)), vol_folder)
+    print("condensing chapters [/]")
+    print("Oneshot detected, finished normaly [/]")
+    os.chdir(main_dirct)
+    shutil.rmtree(folder)
+    exit()
 
 if sum(volumes) == 0:
     if input("Do you want to add all chapters into a single file? (y/n) :  ") == "n":
@@ -16,6 +30,7 @@ if sum(volumes) == 0:
         shutil.rmtree(folder)
         shutil.rmtree(vol_folder)
         exit()
+    print("condensing chapters [/]")
     os.chdir(main_dirct)
     os.chdir(chap_pdf_location)
     result_1 = fitz.open()
@@ -25,7 +40,7 @@ if sum(volumes) == 0:
     os.chdir(main_dirct)
     os.chdir(vol_folder)
     result_1.save("no_volume_{}.pdf".format(Title))
-    print("Volumes not named, finished normaly.")
+    print("Volumes not named, finished normaly [/]")
     os.chdir(main_dirct)
     shutil.rmtree(folder)
     exit()
@@ -41,6 +56,7 @@ if "" or "0" in volumes:
             result_1.insert_pdf(mfile)
     os.chdir(main_dirct)
     os.chdir(vol_folder)
+    print("condensing chapters without a volume [/]")
     result_1.save("Volume_{}.pdf".format("0"))
 
 #this code condenses all the chapters from the same volume into a single file
@@ -54,10 +70,11 @@ for i in range (0,max(volumes)):
             result_1.insert_pdf(mfile)
     os.chdir(main_dirct)
     os.chdir(vol_folder)
+    print("condesning chapters in volume {} [/]".format(i + 1))
     result_1.save("{}_Vol.{}.pdf".format(Title, i + 1))
 
 print("Volumes_condensed [/]")
-print("finished normaly")
+print("finished normaly [/]")
 os.chdir(main_dirct)
 #deletes extra stuff
 shutil.rmtree(folder)
--- secoundary.py
+++ secoundary.py
@@ -3,20 +3,28 @@ import mangadex_downloader
 import os
 from PIL import Image
 import shutil
-
 print("welcome")
 Title = input("what manga would you like to search for \n   :  ")
-folder = (Title + "_manga_raws")
+folder = (Title + "_raws")
 #_________________________________________________________
      
+try:
+    os.mkdir(folder)
+except:
+    print("manga already exists! [X]")
+    if input("delete manga? (y/n)\n   :  ") == "y":
+        shutil.rmtree(folder)
+        os.mkdir(folder)
+    else:
+        exit()
 
-os.mkdir(folder)
 os.system('mangadex-dl "{}" --search --folder "{}" ' .format(Title,folder))
 print("---------------------------------------------------------------------")
 folder_2_location = (folder + "\\{}" .format(os.listdir(folder)[0]))
-
 volumes = []
 chapter_folders = os.listdir(folder_2_location)
+
+shutil.copyfile(folder_2_location + "\\cover.jpg", (folder_2_location + "\\{}\\cover.jpg".format(chapter_folders[-1])))
 chapter_folders.remove("cover.jpg")
 chapter_folders.remove("download.db")
 print("file_system [/]")
@@ -24,11 +32,20 @@ print("file_system [/]")
 os.mkdir(folder_2_location + "\\{}_chapters" .format(Title))
 chap_pdf_location = (folder_2_location + "\\{}_chapters" .format(Title))
 for i in range (0, (len(chapter_folders))  ):
+    
     chap_folder_locaion = (folder_2_location + "\\{}".format(chapter_folders[i]))
-    (os.listdir(chap_folder_locaion))
+    image_list = os.listdir(chap_folder_locaion)
+    image_list.remove("cover.jpg")
+    if "Vol." in chapter_folders[i]:
+        if str.split("Vol.",1)[1] == 1:
+            image_list.insert(0, "cover.jpg")
+    elif "Oneshot" in chapter_folders[i]:
+        image_list.insert(0, "cover.jpg")
+    
     images = [
     Image.open(chap_folder_locaion + "\\" + f).convert('RGB')
-    for f in os.listdir(chap_folder_locaion)]
+    for f in image_list]
+    print(images)
     vol = ""
     str = chapter_folders[i]
     if "Vol." in chapter_folders[i]:
@@ -38,42 +55,41 @@ for i in range (0, (len(chapter_folders))  ):
     if "Ch." in chapter_folders[i]:
         curr_chapter = (str.split("Ch.",1)[1])
         curr_chapter = curr_chapter.strip()
-    pdf_path = chap_pdf_location + ("\\{}_Chapter_{}{}.pdf" .format(Title, curr_chapter,vol))
+    if "Oneshot" in chapter_folders[i]:
+        curr_chapter = "Oneshot"
+        pdf_path = chap_pdf_location + ("\\{}_Oneshot.pdf" .format(Title))
+    else:
+        pdf_path = chap_pdf_location + ("\\{}_Chapter__{}__{}.pdf" .format(Title, curr_chapter,vol))
     try:
         images[0].save(pdf_path, "PDF" ,resolution=100.0, save_all=True, append_images=images[1:])
     except:
-        print("chapter {} failed to compile".format(i))
+        print("chapter {} failed to compile [X]".format(curr_chapter))
+    print("chapter {} compiled [/]".format(curr_chapter))
 #_________________________________________________________
 
 
-
-chap_premerge = os.listdir(chap_pdf_location)
-
+try:
+    chap_premerge = os.listdir(chap_pdf_location)
+    print("chapters found [/]")
+    chap_premerge = sorted(chap_premerge, key = lambda x: float(x.split("__")[1]))
+    print("chapters sorted [/]")
+except IndexError:
+    print("This either a oneshot or somthing has gone wrong [/]")
+    oneshot_ = True
 chap_dict = {}
 
 for i in range (0, len(chap_premerge)):
-    chap_dict[chap_premerge[i]] = chap_premerge[i].split(".")[1]
+    try:
+        chap_dict[chap_premerge[i]] = chap_premerge[i].split("_vol.")[1]
+    except IndexError:
+        print("volume not found when sorting [-]")
+        chap_dict[chap_premerge[i]] = 0
 print("chapters_sorted [/]")
 
 
 
-with open(folder_2_location + "\\Data.txt", 'w') as f:
-    f.write('Variables (if main crashes)\n volumes: {},\n chaptdict: {},\n chap_pdf_location: {},\n Title: {},\n chap_premerge {},\n folder: {},\n folder_2_location: {}'.format(volumes, chap_dict, chap_pdf_location, Title, chap_premerge, folder, folder_2_location))
+with open(folder_2_location + "\\Data.py", 'w') as f:
+    f.write('#Variables (if main crashes)\nvolumes = {}\nchaptdict = {}\nchap_pdf_location = "{}"\nTitle = "{}"\nchap_premerge = {}\nfolder = "{}"\nfolder_2_location = "{}"'.format(volumes, chap_dict, chap_pdf_location, Title, chap_premerge, folder, folder_2_location))
 
 
-
-"""
-os.chdir(chap_pdf_location)
-import fitz
-
-for i in range (0,max(volumes)):
-    result_1 = fitz.open()
-
-    chap_merging = [k for k, v in chap_dict.items() if v == str(i + 1)]
-    for pdf in chap_merging:
-        with fitz.open(pdf) as mfile:
-            result_1.insert_pdf(mfile)
-        
-    result_1.save("Volume_{}.pdf".format(i + 1))
-"""
 

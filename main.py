5272c12b2d7a9b3678a897da477e28e636b3eb0b
Ethan van Wijk <ethanvanwijknz@gmail.com>
2024-12-07 09:18:13 +1300

    update

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from colorama import Fore, Back, Style
 from art import *
+import os
 #libraries, os, requests, colorama, art 
-def prCyan(skk): print("\033[96m {}\033[00m" .format(skk))
-tprint("> KOBO PINE")
-print(Fore.GREEN +"                /\ \n               /  \ \n              /    \ \n             /      \ \n             ⎺/    \⎺ \n             /      \ \n            /        \ \n           /          \ \n           ⎺⎺/      \⎺⎺ \n            /        \ \n           /          \ \n          /            \ \n          ⎺⎺⎺⎺⎺|__|⎺⎺⎺⎺⎺ \n\n")
-prCyan("     > welcome To Kobo Pine manga donwloader for Ebook and other uses")
-prCyan("     > would you like to (1) download a single manga or (2) download manga using a mangadex list")
-print(Fore.RED + "      > mangadex lists may take up to an hour to update after created or edited")
-ans = input("\033[96m {}\033[00m" .format("     > "))
+dir_path = os.path.dirname(os.path.realpath(__file__))
+print(dir_path)
 
+def prCyan(skk): print("\033 {}\033" .format(skk))
+tprint(" KOBO PINE")
+print("                /\ \n               /  \ \n              /    \ \n             /      \ \n             ⎺/    \⎺ \n             /      \ \n            /        \ \n           /          \ \n           ⎺⎺/      \⎺⎺ \n            /        \ \n           /          \ \n          /            \ \n          ⎺⎺⎺⎺⎺|__|⎺⎺⎺⎺⎺ \n\n")
+print("     > welcome To Kobo Pine manga donwloader for Ebook and other uses")
+print("     > would you like to (1) download a single manga or (2) download manga using a mangadex list")
+print("     > mangadex lists may take up to an hour to update after created or edited")
+ans = input("{}" .format("    > "))
 if ans == "1":
-    exec(open("./manga_dld.py").read())
+    exec(open("C:/Users/Ethan/Documents/GitHub/kobo-pine/manga_dld.py").read())
 elif ans == "2":
     exec(open("./manga_list_dld.py").read())
